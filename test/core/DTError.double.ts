cb39853bbe0c596771950c36bc32ee079d984a74
fponthus <fponthus@amiltone.com>
2022-12-21 21:33:24 +0100

    Finishing migration on DTError.spec.ts

--- test/core/DTError.double.ts
+++ test/core/DTError.double.ts
@@ -1,5 +1,4 @@
-import { DTError } from '../../src';
-import { DTComponentStub } from './DTComponent.double';
+import {DTError} from '../../src';
 import {expect} from "@jest/globals";
 import {MockedFunction} from "ts-jest";
 import DYOToolsComponent from "../../src/core/DTComponent";
@@ -10,6 +9,44 @@ import DYOToolsComponent from "../../src/core/DTComponent";
 export const CodeTest = 'DTError-code-123456';
 export const MessageTest = 'DTError-message-test';
 export const TimestampTest = 1500000000000;
+
+/******************** HELPER TEST CLASS
+ * Helper test class, inherits the main component
+ * Providing methods to property access and other facilities, in order to avoid using class methods
+ * *****/
+export class DTErrorTest extends DTError {
+  th_get_code(): string {
+    return this.code;
+  }
+
+  th_get_message(): string {
+    return this.message;
+  }
+
+  th_get_timestamp(): Date {
+    return this.timestamp;
+  }
+
+  th_set_timestamp(timestamp: Date): void {
+    this.timestamp = timestamp;
+  }
+
+  th_get_initiator(): DYOToolsComponent {
+    return this.initiator;
+  }
+
+  th_set_initiator(initiator: DYOToolsComponent): void {
+    this.initiator = initiator;
+  }
+
+  th_get_convicted(): DYOToolsComponent {
+    return this.convicted;
+  }
+
+  th_set_convicted(convicted: DYOToolsComponent): void {
+    this.convicted = convicted;
+  }
+}
 
 /******************** STUB CLASS
  * Stub class, for using in other component
@@ -34,20 +71,6 @@ export class DTErrorStub extends DTError {
     return this.timestamp;
   }
 }
-
-// // Mock Constructor for DTError
-// export class DTErrorMock extends DTError {
-//   constructor(setComponents = false) {
-//     super('', '');
-//     this.code = CodeTest;
-//     this.message = MessageTest;
-//     this.timestamp = new Date(TimestampTest);
-//     if (setComponents) {
-//       this.initiator = new DTComponentStub('initiator');
-//       this.convicted = new DTComponentStub('convicted');
-//     }
-//   }
-// }
 
 /******************** HELPER METHODS
  * Additional helper methods to use with testing
--- test/core/DTError.spec.ts
+++ test/core/DTError.spec.ts
@@ -1,17 +1,13 @@
-import {
-  afterEach, beforeEach, describe, expect, jest, test,
-} from '@jest/globals';
-import {
-  CodeTest, DTErrorMock, MessageTest, TimestampTest,
-} from './DTError.double';
-import { DTError } from '../../src';
-import { DTComponentStub, IDTest as ComponentIdTest } from './DTComponent.double';
+import {afterEach, beforeEach, describe, expect, jest, test,} from '@jest/globals';
+import {CodeTest, DTErrorTest, MessageTest, TimestampTest,} from './DTError.double';
+import {DTComponentStub, IDTest as ComponentIdTest} from './DTComponent.double';
 
+/************************* TESTS SUITES *******************************/
 describe('class DYOToolsError', () => {
-  let errorMock: DTErrorMock;
+  let errorTest: DTErrorTest;
 
   beforeEach(() => {
-    errorMock = new DTErrorMock();
+    errorTest = new DTErrorTest(CodeTest, MessageTest);
   });
 
   afterEach(() => {
@@ -20,87 +16,74 @@ describe('class DYOToolsError', () => {
 
   describe('constructor()', () => {
     test('creation with standard field (code, message and timestamp)', () => {
-      const newError = new DTError(CodeTest, MessageTest);
+      const newError = new DTErrorTest(CodeTest, MessageTest);
       const currentDate = new Date();
-      jest.spyOn(newError, 'getCode').mockImplementation(function () {
-        return this.code;
-      });
-      jest.spyOn(newError, 'getMessage').mockImplementation(function () {
-        return this.message;
-      });
-      jest.spyOn(newError, 'getTimestamp').mockImplementation(function () {
-        return this.timestamp;
-      });
-      jest.spyOn(newError, 'getInitiator').mockImplementation(function () {
-        return this.initiator;
-      });
-      jest.spyOn(newError, 'getConvicted').mockImplementation(function () {
-        return this.convicted;
-      });
 
-      expect(newError.getCode()).toBe(CodeTest);
-      expect(newError.getMessage()).toBe(MessageTest);
-      expect(newError.getTimestamp().toDateString()).toBe(currentDate.toDateString());
-      expect(newError.getTimestamp().toTimeString()).toBe(currentDate.toTimeString());
-      expect(newError.getInitiator()).toBeUndefined();
-      expect(newError.getConvicted()).toBeUndefined();
+      expect(newError.th_get_code()).toBe(CodeTest);
+      expect(newError.th_get_message()).toBe(MessageTest);
+      expect(newError.th_get_timestamp().toDateString()).toBe(currentDate.toDateString());
+      expect(newError.th_get_timestamp().toTimeString()).toBe(currentDate.toTimeString());
+      expect(newError.th_get_initiator()).toBeUndefined();
+      expect(newError.th_get_convicted()).toBeUndefined();
     });
 
     test('creation with initiator component', () => {
       const initiatorMock = new DTComponentStub('initiator');
-      const newError = new DTError(CodeTest, MessageTest, initiatorMock);
-      jest.spyOn(newError, 'getInitiator').mockImplementation(function () {
-        return this.initiator;
-      });
+      const newError = new DTErrorTest(CodeTest, MessageTest, initiatorMock);
 
-      expect(newError.getInitiator().getId()).toBe(`${ComponentIdTest}-initiator`);
+      expect(newError.th_get_initiator().getId()).toBe(`${ComponentIdTest}-initiator`);
     });
 
     test('creation with initiator and convicted component', () => {
       const initiatorMock = new DTComponentStub('initiator');
       const convictedMock = new DTComponentStub('convicted');
-      const newError = new DTError(CodeTest, MessageTest, initiatorMock, convictedMock);
-      jest.spyOn(newError, 'getConvicted').mockImplementation(function () {
-        return this.convicted;
-      });
+      const newError = new DTErrorTest(CodeTest, MessageTest, initiatorMock, convictedMock);
 
-      expect(newError.getConvicted().getId()).toBe(`${ComponentIdTest}-convicted`);
+      expect(newError.th_get_convicted().getId()).toBe(`${ComponentIdTest}-convicted`);
     });
   });
 
   describe('getCode()', () => {
     test('return code property', () => {
-      expect(errorMock.getCode()).toBe(CodeTest);
+      expect(errorTest.getCode()).toBe(CodeTest);
     });
   });
 
   describe('getMessage()', () => {
     test('return message property', () => {
-      expect(errorMock.getMessage()).toBe(MessageTest);
+      expect(errorTest.getMessage()).toBe(MessageTest);
     });
   });
 
   describe('getTimestamp()', () => {
     test('return timestamp property', () => {
-      expect(errorMock.getTimestamp().getTime()).toBe(TimestampTest);
+      errorTest.th_set_timestamp(new Date(TimestampTest));
+
+      expect(errorTest.getTimestamp().getTime()).toBe(TimestampTest);
     });
   });
 
   describe('getInitiator()', () => {
-    test('return initiator component', () => {
-      const errorMockSet = new DTErrorMock(true);
+    test('return empty initiator component by default', () => {
+      expect(errorTest.getInitiator()).toBeUndefined();
+    });
 
-      expect(errorMock.getInitiator()).toBeUndefined();
-      expect(errorMockSet.getInitiator().getId()).toBe(`${ComponentIdTest}-initiator`);
+    test('return initiator component when set', () => {
+      errorTest.th_set_initiator(new DTComponentStub('initiator'));
+
+      expect(errorTest.getInitiator().getId()).toBe(`${ComponentIdTest}-initiator`);
     });
   });
 
   describe('getConvicted()', () => {
-    test('return convicted component', () => {
-      const errorMockSet = new DTErrorMock(true);
+    test('return empty convicted component by default', () => {
+      expect(errorTest.getConvicted()).toBeUndefined();
+    });
 
-      expect(errorMock.getConvicted()).toBeUndefined();
-      expect(errorMockSet.getConvicted().getId()).toBe(`${ComponentIdTest}-convicted`);
+    test('return convicted component when set', () => {
+      errorTest.th_set_convicted(new DTComponentStub('convicted'));
+
+      expect(errorTest.getConvicted().getId()).toBe(`${ComponentIdTest}-convicted`);
     });
   });
 });
